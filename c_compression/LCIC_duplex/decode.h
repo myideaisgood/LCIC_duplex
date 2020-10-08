2c5b0f5c5c14f4a6b74bbc1e9e599821106ae521
hochang <myideaisgood@hanmail.net>
2020-10-08 14:48:10 +0900

    exe commit
    exe commit

--- c_compression/LCIC_duplex/decode.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-float runDecoder(char *codefile_y, char *codefile_u, char *codefile_v, char *outfile, char *weight_smooth_y, char *weight_smooth_u, char *weight_smooth_v, char* weight_texture_y, char* weight_texture_u, char* weight_texture_v);
--- python_weights_training/main.py
+++
@@ -1,8 +0,0 @@
-from config import parse_args
-from network import Network    
-    
-args = parse_args()
-my_net = Network(args)
-my_net.build()
-my_net.train()
-my_net.print_all_weights()
